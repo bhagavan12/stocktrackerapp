9f8784ba3db52a83103458c9e78861d9c9817bba
bhagavan12 <jsbteja123@gmail.com>
2025-01-13 21:33:50 +0530

    first commit

---
+++ src/main/java/com/klef/stocktracker/Stocktracker5Application.java
@@ -0,0 +1,13 @@
+package com.klef.stocktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stocktracker5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stocktracker5Application.class, args);
+	}
+
+}
